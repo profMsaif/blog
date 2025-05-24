cc9a8c35fbedb3715f551126e7188e865288abe6
profMsaif <profmsaif@gmail.com>
2025-05-24 15:01:32 +0500

    Fix celery

--- Blog/celery.py
+++ Blog/celery.py
@@ -3,6 +3,6 @@ from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE","Blog.settings")
 
-app = Celery("blog")
+app = Celery("Blog")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
--- blog_app/views/post.py
+++ blog_app/views/post.py
@@ -3,7 +3,7 @@ from django.views import generic
 
 from blog_app.forms import CommentForm
 from blog_app.models import Post
-from blog_app.tasks import email_tasks
+from blog_app.tasks.email_tasks import send_welcome_email
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by("-created_on")
@@ -16,7 +16,8 @@ class PostList(generic.ListView):
 
 
 def post_detail(request, slug):
-    email_tasks.send_welcome_email("all user")
+    # apply_async will runs the task on worker via redis ( enqueue task to redis)
+    send_welcome_email.apply_async(("all user",))
     template_name = "blog_app/post_detail.html"
     post = get_object_or_404(Post, slug=slug)
     comments = post.comments.filter(active=True)
